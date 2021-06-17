410cef8e4179d28e05ff5d675889e3c0dcb00ba9
Your Name <you@example.com>
2021-06-17 04:58:45 -0700

    ALX Low Level Programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+  * main - Prints the sizeof many var types
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
