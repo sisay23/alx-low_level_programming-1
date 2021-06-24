048a6321c08a3157bb4a3e7f7f83d352ddb03229
Your Name <you@example.com>
2021-06-24 06:29:25 -0700

    C - Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,4 @@
+  
 #include "holberton.h"
 
 /**
@@ -7,11 +8,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i =0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
 
