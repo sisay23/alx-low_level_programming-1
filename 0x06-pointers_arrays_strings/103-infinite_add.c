647db8061e6b70b9d29d0181225c4ff203b806d4
Your Name <you@example.com>
2021-07-01 04:42:43 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,49 +1,54 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * infinite_add - adds two numbers
- * @n1: number 1
- * @n2: number 2
- * @r: buffer to store result
- * @size_r: size of buffer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * infinite_add - this functions add two numbers
+(* a blank line
+ *@n1: first number to add
+ *@n2: second number to add
+ *@r: this is the buffer where the function store the result
+ *@size_r: this is the size of the buffer
+* Description: this function adds two numbers)?
+(* section header: the header of this function is holberton.h)*
+* Return: this return a char.
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, diff, big, small, cyc, cyc2, count, tmp, ten, check;
-	char *less, *more;
+	int n, i, j, k, l, m, add, rest, d1, d2;
 
-	i = 0, j = 0;
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i >= j)
-		big = i, small = j, diff = i - j + 1, less = n2, more = n1;
-	else
-		big = j, small = i, diff = j - i + 1, less = n1, more = n2;
-	count = 0, cyc = diff, ten = 0;
-	for (count = 0; count < small; count++, cyc++)
-		r[cyc] = less[count];
-	for (cyc2 = big; cyc2 >= 1; cyc2--)
+	l = k = rest =  0;
+	for (i = 0; n1[i] != '\0'; i++)
+		;
+	for (j = 0; n2[j] != '\0'; j++)
+		;
+	if (i + 2 > size_r || j + 2 > size_r)
+		return (0);
+	i--, j--;
+	while (i >= 0 || j >= 0)
 	{
-		if (cyc > size_r - 1)
-			return (0);
-		if (r[cyc2] <= 57 && r[cyc2] >= 48)
-			tmp = (r[cyc2] - '0') + (more[cyc2 - 1] - '0') + ten;
+		d1 = 0;
+		d2 = 0;
+		if (i >= 0)
+			d1 = n1[i--] - '0';
+		if (j >= 0)
+			d2 = n2[j--] - '0';
+		add = d1 + d2 + rest;
+		if (add > 9)
+		{
+			rest = 1;
+			add = add - 10;
+		}
 		else
-			tmp = (more[cyc2 - 1] - '0') + ten;
-		ten = tmp / 10;
-		tmp = tmp - (ten * 10);
-		r[cyc2] = '0' + tmp;
+			rest = 0;
+		r[k++] = (add + '0');
 	}
-	r[cyc2] = '0' + ten;
-	if (ten == 0)
-		r++;
-	for (check = 0; check < size_r; check++)
-		if (r[check] > 57 || r[check] < 48)
-			r[check] = 0;
+	if (rest == 1)
+		r[k++] = (1 + '0');
+	m = k,	k--;
+	for (l = 0; l < k; l++, k--)
+	{
+		n = r[k];
+		r[k] = r[l];
+		r[l] = n;
+	}
+	r[m] = '\0';
 	return (r);
 }
