a0b494aa471bb608b4214e29441e49905c78c1ea
Your Name <you@example.com>
2021-07-01 04:40:46 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,3 +10,4 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
