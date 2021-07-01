e372857dc2af71d2db4641c50956b91e9096532d
Your Name <you@example.com>
2021-07-01 04:20:06 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- *
- * Return: address of r or 0
+ * infinite_add - adds two numbers.
+ * @n1:char
+ * @n2:char.
+ * @r:char
+ * @size_r:int
+ * Return:char.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
 		;
