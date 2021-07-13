ec757db4aee550cdea4c5f090ac75b553eac12dd
Your Name <you@example.com>
2021-07-13 03:27:11 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,57 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strlen - returns the length of the string
- * @s: input string to count
- * Description: returns the length of a given string
- * Return: length of string as int
- **/
-int _strlen(char *s)
+* lenght - Entry point
+* @string: int
+* Return: Always 0 (Success)
+*/
+int lenght(char *string)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i + 1);
+	return ((*string == '\0') ? 1 : 1 + lenght(string + 1));
 }
 /**
- * argstostr - concatenates all arguments of program
- * @ac: number of arguments
- * @av: arguments, pointer to strings
- * Description: concatenate all args to one string, separated by \n
- * Return: pointer to string, NULL if fails
- **/
+* argstostr - Entry point
+* @ac: int
+* @av: char
+* Return: Always 0 (Success)
+*/
 char *argstostr(int ac, char **av)
 {
-	char *dest;
-	unsigned int size, i, j, k;
+	int i, j, k, lenght_av = 0;
+	char *string;
 
-	size = 0;
+	if (av == NULL || ac == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		lenght_av += lenght(*(av + i));
+
+	string = (char *)malloc((lenght_av + 1) * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+
 	k = 0;
-
-	if (ac <= 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < (unsigned int)ac; i++)
-	{
-		size += _strlen(av[i]);
-	}
-
-	dest = (char *)malloc((size + 1) * sizeof(char));
-
-	if (dest == NULL)
-		return (NULL);
-
-	for (i = 0; i < (unsigned int)ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
+	for (i = 0; i < ac; i++)
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
 		{
-			dest[k] = av[i][j];
-			k++;
+			*(string + k) = *(*(av + i) + j);
+			if (*(*(av + i) + (j + 1)) == '\0')
+			{
+				*(string + k + 1) = '\n';
+				k += 1;
+			}
+			k += 1;
 		}
-		dest[k] = '\n';
-		k++;
-	}
-	dest[k] = '\0';
-	return (dest);
+	*(string + k) = '\0';
+	return (string);
 }
+
