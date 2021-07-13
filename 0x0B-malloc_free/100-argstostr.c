95d34d6a19b077d0d60035f9966f5254c9d62229
Your Name <you@example.com>
2021-07-13 03:20:36 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,58 @@
-/*
- * File: 5-argstostr.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * argstostr - Concatenates all arguments of the program into a string;
- *             arguments are separated by a new line in the string.
- * @ac: The number of arguments passed to the program.
- * @av: An array of pointers to the arguments.
- *
- * Return: If ac == 0, av == NULL, or the function fails - NULL.
- *         Otherwise - a pointer to the new string.
- */
+ * _strlen - returns the length of the string
+ * @s: input string to count
+ * Description: returns the length of a given string
+ * Return: length of string as int
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i + 1);
+}
+/**
+ * argstostr - concatenates all arguments of program
+ * @ac: number of arguments
+ * @av: arguments, pointer to strings
+ * Description: concatenate all args to one string, separated by \n
+ * Return: pointer to string, NULL if fails
+ **/
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int arg, byte, index, size = ac;
+	char *dest;
+	unsigned int size, i, j, k;
 
-	if (ac == 0 || av == NULL)
+	size = 0;
+	k = 0;
+
+	if (ac <= 0 || av == NULL)
 		return (NULL);
 
-	for (arg = 0; arg < ac; arg++)
+	for (i = 0; i < (unsigned int)ac; i++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			size++;
+		size += _strlen(av[i]);
 	}
 
-	str = malloc(sizeof(char) * size + 1);
+	dest = (char *)malloc((size + 1) * sizeof(char));
 
-	if (str == NULL)
+	if (dest == NULL)
 		return (NULL);
 
-	index = 0;
-
-	for (arg = 0; arg < ac; arg++)
+	for (i = 0; i < (unsigned int)ac; i++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			str[index++] = av[arg][byte];
-
-		str[index++] = '\n';
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			dest[k] = av[i][j];
+			k++;
+		}
+		dest[k] = '\n';
+		k++;
 	}
-
-	str[size] = '\0';
-
-	return (str);
+	dest[k] = '\0';
+	return (dest);
 }
