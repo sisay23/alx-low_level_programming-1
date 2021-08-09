632d6e5fca98d8bf24bf0ae84dc91a7f4b6c39dc
Your Name <you@example.com>
2021-08-09 04:19:44 -0700

    C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, _write, i; /* i: is the len of the content */
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	close(o);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
 
+		_write = write(fd, text_content, i);
+		if (_write == -1)
+			return (-1);
+	}
+	close(fd);
 	return (1);
 }
