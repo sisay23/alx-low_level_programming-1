66e7c6654747cb8d061eeb43c9e799fbb33a983d
Your Name <you@example.com>
2021-09-22 05:47:15 -0700

    C - Dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+#gcc -fPIC -c *.c
+#gcc -shared -o liball.so *.o
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
